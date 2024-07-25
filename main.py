8e8002651745561ae22b036252482e3487e9404c
sohamb37 <101194732+sohamb37@users.noreply.github.com>
2024-07-26 01:09:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from website import create_app
+from flask import render_template
+
+app = create_app()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 8222)
+
